0df68de2e5b2501282f0ede3b54bbf4f2e8b7c12
mtrimner <matt.trimner@gmail.com>
2020-11-24 13:51:22 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+
+    let configObj = {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+      };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        let bodyAppend = document.querySelector('body')
+        bodyAppend.innerHTML = `${object["id"]}`
+    })
+    .catch(function(error) {
+        alert("Unauthorized Access")
+        let bodyAppend = document.querySelector('body')
+        bodyAppend.innerHTML = `${error.message}`
+    });
+
+}
+
+   
+  
